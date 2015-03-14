98f9994970e555cb27ed76232b09d62d5de6f96d
listik-cveta <adv3rtis@gmail.com>
2015-03-14 16:31:48 +0300

    Added dependent 'destroy' on question model

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Question, :type => :model do
   #pending "add some examples to (or delete) #{__FILE__}"
-  it { should have_many(:answers) }
+  it { should have_many(:answers).dependent(:destroy) }
   it { should validate_presence_of :title}
   it { should validate_presence_of :body}
   it { should validate_length_of(:body).is_at_least(10) }
