d3d8ed71e14eceb4193c15a62c477247399d3625
listik-cveta <adv3rtis@gmail.com>
2015-03-14 13:46:59 +0300

    Added validations of length and covered by tests succsesfull

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -4,4 +4,5 @@ RSpec.describe Answer, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
     it { should validate_presence_of :body }
     it { should belong_to :question }
+    it { should validate_length_of(:body).is_at_least(10) }
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,15 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Question, :type => :model do
   #pending "add some examples to (or delete) #{__FILE__}"
-  it {should validate_presence_of :title}
-  it {should validate_presence_of :body}
-
-  # it 'validates presence of title' do
-  #   expect(Question.new(body: '123')).to_not be_valid
-  # end
-  #
-  # it 'validates presence of body' do
-  #   expect(Question.new(title: '123')).to_not be_valid
-  # end
-
+  it { should have_many(:answers) }
+  it { should validate_presence_of :title}
+  it { should validate_presence_of :body}
+  it { should validate_length_of(:body).is_at_least(10) }
+  it { should validate_length_of(:title).is_at_most(70) }
 end
