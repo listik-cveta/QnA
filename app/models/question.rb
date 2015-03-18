2ab2f3a2fc19d5bb379c52a1133d40a5afe8dc3c
listik-cveta <adv3rtis@gmail.com>
2015-03-18 11:41:01 +0300

    Created question controller with actions, covered by tests

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ActiveRecord::Base
   has_many :answers, dependent: :destroy
-  validates :title, :body, presence: true, length: {minimum: 10, message: "is too short (minimum is 10 characters)"}
-  validates :title, length: { maximum: 70, message: "is too long (maximum is 70 characters)" }
+  validates :title, :body, presence: true#, length: {minimum: 10, message: "is too short (minimum is 10 characters)"}
+  #validates :title, length: { maximum: 70, message: "is too long (maximum is 70 characters)" }
 end
