1b209dd40ed2098e7745d3a368beb8e7673ee1df
listik-cveta <adv3rtis@gmail.com>
2015-03-22 22:18:31 +0300

    Added answers controller, that can create answer to his question; covered by tests

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action load_question, only: [:show, :edit, :create]
+  before_action :load_question, only: [:show, :edit, :update, :destroy]
 
   def index
     @questions = Question.all
@@ -22,16 +22,29 @@ class QuestionsController < ApplicationController
     else
       render :new
     end
+  end
 
-    private
-
-    def load_question
-      @question = Question.find(params[:id])
-    end
-
-    def question_params
-      params.require(:question).permit(:title, :body)
+  def update
+    if @question.update(question_params)
+    redirect_to @question
+    else
+      render :edit
     end
   end
+
+  def destroy
+    @question.destroy
+    redirect_to question_path
+  end
+
+  private
+
+  def load_question
+    @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:title, :body)
+  end
 end
-#end
+
